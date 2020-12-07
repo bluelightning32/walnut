722a312c7aff396a366dd99c9854e5199097c2f0
Kyle Stemen <bluelightning32@gmail.com>
2020-12-06 16:41:24 -0800

    Add a unit test for Get2DTwistDirReduced

--- test/point3_test.cpp
+++ test/point3_test.cpp
@@ -46,4 +46,30 @@ TEST(Point3, Get2DTwistDirDropZ) {
   }
 }
 
+TEST(Point3, Get2DTwistDirReducedDropZ) {
+  int z_values[] = {0, 10, -10};
+  for (int z : z_values) {
+    //
+    // b
+    // ^
+    // |
+    // c ---> a
+    //
+    Point3<> a(1, 0, z);
+    Point3<> center(0, 0, z);
+    Point3<> b(0, 1, z);
+
+    // a to b is counter-clockwise
+    EXPECT_EQ(center.Get2DTwistDirReduced(/*drop_dimension=*/2, a, b), 1);
+    // b to a is clockwise
+    EXPECT_EQ(center.Get2DTwistDirReduced(/*drop_dimension=*/2, b, a), -1);
+
+    // collinear tests
+    EXPECT_EQ(center.Get2DTwistDirReduced(/*drop_dimension=*/2, a, a), 0);
+    EXPECT_EQ(center.Get2DTwistDirReduced(/*drop_dimension=*/2, b, b), 0);
+    EXPECT_EQ(center.Get2DTwistDirReduced(/*drop_dimension=*/2, a, center), 0);
+    EXPECT_EQ(center.Get2DTwistDirReduced(/*drop_dimension=*/2, center, a), 0);
+  }
+}
+
 }  // walnut
