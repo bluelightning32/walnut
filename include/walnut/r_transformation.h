5e1e2071bf836324d268a83dbb9eb2bfa86eebd7
Kyle Stemen <bluelightning32@gmail.com>
2020-12-27 12:29:19 -0800

    Add a function to compare bivectors after the R transformation

---
+++ include/walnut/r_transformation.h
@@ -0,0 +1,67 @@
+#ifndef WALNUT_R_TRANFORMATION_H__
+#define WALNUT_R_TRANFORMATION_H__
+
+// In this context, if p is reverse lexicographically less than q, that means:
+//   p.z < q.z ||
+//   p.y < q.y ||
+//   p.x < q.x
+//
+// R transformation is an R^3 shear transformation. The name is short for
+// reverse lexicographical transformation. For to points, p and q, iff. p is
+// reverse lexicographically less than q, then (R p).z < (R q).z.
+//
+// The R transform takes a parameter, 0 < epsilon < 1, which should be small
+// enough that comparisons of R-transformed values stabilize.
+
+#include "walnut/homo_point3.h"
+#include "walnut/vector3.h"
+
+namespace walnut {
+
+// R transforms a point. Epsilon is specified by 1/eps_inv.
+//
+// Since this function is likely to overflow the HomoPoint3 precision, this
+// function should only be used for testing purposes.
+template <int num_bits, int denom_bits>
+HomoPoint3<num_bits, denom_bits> RTransform(
+    const HomoPoint3<num_bits, denom_bits>& p,
+    const BigInt<denom_bits>& eps_inv) {
+  // Add eps^2 to x and eps to y.
+  BigInt<denom_bits> eps_inv_squared(eps_inv * eps_inv);
+  return HomoPoint3<num_bits, denom_bits>(
+      BigInt<num_bits>(p.x()*eps_inv_squared + p.w()),
+      BigInt<num_bits>(p.y()*eps_inv_squared + eps_inv * p.w()),
+      BigInt<num_bits>((p.z()*eps_inv + p.y())*eps_inv + p.x()),
+      BigInt<denom_bits>(p.w()*eps_inv_squared));
+}
+
+// Applies the R transform to two bivectors and compares the result in the XY
+// plane.
+//
+// This function should not be used for the vector from the origin of a Point3,
+// because that is a contravariant vector, not a bivector.
+//
+// This function can be used on HalfSpace3 normals, because they are bivectors.
+//
+// * Returns <0 if (u R) is clockwise from (v R)
+// * Returns 0 if u points in the same or opposite direction as v (as do (u R)
+//   and (v R)).
+// * Returns >0 if (u R) is counter-clockwise from (v R)
+template <int component_bits>
+int RXYCompareBivector(const Vector3<component_bits>& u,
+                       const Vector3<component_bits>& v) {
+  int comparexy = (u.x()*v.y()).Compare(u.y()*v.x());
+  if (comparexy != 0) {
+    return comparexy;
+  }
+  int comparexz = (u.z()*v.x()).Compare(u.x()*v.z());
+  if (comparexz != 0) {
+    return comparexz;
+  }
+  int compareyz = (u.y()*v.z()).Compare(u.z()*v.y());
+  return compareyz;
+}
+
+}  // walnut
+
+#endif // WALNUT_R_TRANFORMATION_H__
---
+++ test/r_transformation_test.cpp
@@ -0,0 +1,152 @@
+#include "walnut/r_transformation.h"
+
+#include "gtest/gtest.h"
+#include "walnut/homo_point3.h"
+#include "walnut/half_space3.h"
+
+namespace walnut {
+
+TEST(RXYCompareBivector, DiagonalPlanes) {
+  //
+  // p3&p4   p1&p2
+  //      \ /
+  //       p0
+  //
+  //        n2
+  //       /
+  //       \
+  //        n1
+  //
+  HomoPoint3<> p[5] = {
+    HomoPoint3<>(1, 1, 5, 1),
+    HomoPoint3<>(2, 2, 5, 1),
+    HomoPoint3<>(2, 2, 6, 1),
+    HomoPoint3<>(0, 2, 5, 1),
+    HomoPoint3<>(0, 2, 6, 1),
+  };
+
+  HalfSpace3<> h1(p[0], p[1], p[2]);
+  EXPECT_GT(h1.x(), 0);
+  EXPECT_LT(h1.y(), 0);
+  EXPECT_EQ(h1.z(), 0);
+
+  HalfSpace3<> h2(p[0], p[3], p[4]);
+  EXPECT_GT(h2.x(), 0);
+  EXPECT_GT(h2.y(), 0);
+  EXPECT_EQ(h2.z(), 0);
+
+  EXPECT_GT(h1.normal().DropDimension(2).Cross(
+        h2.normal().DropDimension(2)), 0);
+
+  HomoPoint3<> p_transformed[5];
+  for (int i = 0; i < 5; ++i) {
+    p_transformed[i] = RTransform(p[i], HomoPoint3<>::DenomInt(10));
+  }
+
+  HalfSpace3<> h1_transformed(p_transformed[0], p_transformed[1],
+                              p_transformed[2]);
+  HalfSpace3<> h2_transformed(p_transformed[0], p_transformed[3],
+                              p_transformed[4]);
+
+  EXPECT_GT(h1_transformed.normal().DropDimension(2).Cross(
+        h2_transformed.normal().DropDimension(2)), 0);
+
+  EXPECT_GT(RXYCompareBivector(h1.normal(), h2.normal()), 0);
+}
+
+TEST(RXYCompareBivector, Y0Normal) {
+  //
+  // p3 p2 p1
+  //   \_ \| 
+  //     - p0
+  //
+  //      n1 ->
+  //      n2 ->
+  //
+  HomoPoint3<> p[4] = {
+    HomoPoint3<>(1, 1, 5, 1),
+    HomoPoint3<>(1, 2, 5, 1),
+    HomoPoint3<>(0, 2, 6, 1),
+    HomoPoint3<>(-1, 2, 6, 1),
+  };
+
+  HalfSpace3<> h1(p[0], p[1], p[2]);
+  EXPECT_GT(h1.x(), 0);
+  EXPECT_EQ(h1.y(), 0);
+  EXPECT_GT(h1.z(), 0);
+
+  HalfSpace3<> h2(p[0], p[1], p[3]);
+  EXPECT_GT(h2.x(), 0);
+  EXPECT_EQ(h2.y(), 0);
+  EXPECT_GT(h2.z(), 0);
+
+  EXPECT_GT(h2.z(), h1.z());
+  EXPECT_NE(h1.normal(), h2.normal());
+
+  HomoPoint3<> p_transformed[4];
+  for (int i = 0; i < 4; ++i) {
+    p_transformed[i] = RTransform(p[i], HomoPoint3<>::DenomInt(10));
+  }
+
+  HalfSpace3<> h1_transformed(p_transformed[0], p_transformed[1],
+                              p_transformed[2]);
+  HalfSpace3<> h2_transformed(p_transformed[0], p_transformed[1],
+                              p_transformed[3]);
+
+  EXPECT_LT(h1_transformed.normal().DropDimension(2).Cross(
+        h2_transformed.normal().DropDimension(2)), 0);
+
+  EXPECT_LT(RXYCompareBivector(h1.normal(), h2.normal()), 0);
+}
+
+TEST(RXYCompareBivector, X0Normal) {
+  //
+  //   p1- p0
+  //   p2/ |
+  //   p3-/
+  //
+  //       ^
+  //       |
+  //      n1
+  //
+  //       ^
+  //       |
+  //      n2
+  //
+  HomoPoint3<> p[4] = {
+    HomoPoint3<>(1, 1, 5, 1),
+    HomoPoint3<>(0, 1, 5, 1),
+    HomoPoint3<>(0, -1, 6, 1),
+    HomoPoint3<>(0, -2, 6, 1),
+  };
+
+  HalfSpace3<> h1(p[0], p[1], p[2]);
+  EXPECT_EQ(h1.x(), 0);
+  EXPECT_GT(h1.y(), 0);
+  EXPECT_GT(h1.z(), 0);
+
+  HalfSpace3<> h2(p[0], p[1], p[3]);
+  EXPECT_EQ(h2.x(), 0);
+  EXPECT_GT(h2.y(), 0);
+  EXPECT_GT(h2.z(), 0);
+
+  EXPECT_GT(h2.z(), h1.z());
+  EXPECT_NE(h1.normal(), h2.normal());
+
+  HomoPoint3<> p_transformed[4];
+  for (int i = 0; i < 4; ++i) {
+    p_transformed[i] = RTransform(p[i], HomoPoint3<>::DenomInt(10));
+  }
+
+  HalfSpace3<> h1_transformed(p_transformed[0], p_transformed[1],
+                              p_transformed[2]);
+  HalfSpace3<> h2_transformed(p_transformed[0], p_transformed[1],
+                              p_transformed[3]);
+
+  EXPECT_GT(h1_transformed.normal().DropDimension(2).Cross(
+        h2_transformed.normal().DropDimension(2)), 0);
+
+  EXPECT_GT(RXYCompareBivector(h1.normal(), h2.normal()), 0);
+}
+
+}  // walnut
