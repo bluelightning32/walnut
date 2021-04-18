1d476b6461134712d288f775417d49fff5259bdb
Kyle Stemen <bluelightning32@gmail.com>
2021-04-18 00:57:13 -0700

    Factor the logic out of ConvexPolygonVertexIterator into TransformIterator

--- include/walnut/convex_polygon_vertex_iterator.h
+++ include/walnut/convex_polygon_vertex_iterator.h
@@ -1,131 +1,29 @@
 #ifndef WALNUT_CONVEX_POLYGON_VERTEX_ITERATOR_H__
 #define WALNUT_CONVEX_POLYGON_VERTEX_ITERATOR_H__
 
-#include <type_traits>
+#include "walnut/homo_point3.h"
+#include "walnut/transform_iterator.h"
 
 namespace walnut {
+
+template <typename Edge>
+struct ReturnVertexFromEdge {
+  const HomoPoint3& operator()(const Edge& edge) const {
+    return edge.vertex();
+  }
+};
 
 // An iterator that produces just the const vertex reference from an input
 // iterator that produces a `ConvexPolygonEdge`.
 template <typename EdgeIterator>
-class ConvexPolygonVertexIterator {
+class ConvexPolygonVertexIterator
+  : public TransformIterator<
+             EdgeIterator,
+             ReturnVertexFromEdge<typename EdgeIterator::value_type>> {
  public:
-  using difference_type = typename EdgeIterator::difference_type;
-  // The extra set of parenthesis around the argument to decltype are necessary
-  // so that the argument is considered an expression instead of an entity.
-  using value_type = const typename std::remove_reference<decltype((
-        std::declval<typename EdgeIterator::value_type>().vertex()))>::type;
-  using pointer = value_type*;
-  using reference = value_type&;
-  using iterator_category = typename EdgeIterator::iterator_category;
-
-  ConvexPolygonVertexIterator() = default;
-
-  explicit ConvexPolygonVertexIterator(const EdgeIterator& edge_iterator) :
-    edge_iterator_(edge_iterator) { }
-
-  template <typename OtherEdgeIterator,
-            typename = std::enable_if_t<
-              std::is_constructible<EdgeIterator, OtherEdgeIterator>::value>>
-  ConvexPolygonVertexIterator(
-      const ConvexPolygonVertexIterator<OtherEdgeIterator>& other) :
-    edge_iterator_(other.edge_iterator_) { }
-
-  // Many of these functions return auto so that SFINAE kicks in and removes
-  // any operators that are not valid due to the input iterator type.
-
-  template <typename OtherEdgeIterator>
-  auto operator==(
-      const ConvexPolygonVertexIterator<OtherEdgeIterator>& other) const {
-    return edge_iterator_ == other.edge_iterator_;
-  }
-
-  template <typename OtherEdgeIterator>
-  auto operator!=(
-      const ConvexPolygonVertexIterator<OtherEdgeIterator>& other) const {
-    return edge_iterator_ != other.edge_iterator_;
-  }
-
-  reference operator*() const {
-    return edge_iterator_->vertex();
-  }
-
-  pointer operator->() const {
-    return &edge_iterator_->vertex();
-  }
-
-  ConvexPolygonVertexIterator& operator++() {
-    ++edge_iterator_;
-    return *this;
-  }
-
-  ConvexPolygonVertexIterator operator++(int) {
-    return ConvexPolygonVertexIterator(edge_iterator_++);
-  }
-
-  auto& operator--() {
-    --edge_iterator_;
-    return *this;
-  }
-
-  auto operator--(int) {
-    return ConvexPolygonVertexIterator(edge_iterator_--);
-  }
-
-  auto operator+(difference_type offset) const {
-    return ConvexPolygonVertexIterator(edge_iterator_ + offset);
-  }
-
-  auto operator-(difference_type offset) const {
-    return ConvexPolygonVertexIterator(edge_iterator_ - offset);
-  }
-
-  template <typename OtherEdgeIterator>
-  auto operator-(
-      const ConvexPolygonVertexIterator<OtherEdgeIterator>& other) const {
-    return edge_iterator_ - other.edge_iterator_;
-  }
-
-  auto& operator+=(difference_type offset) {
-    return edge_iterator_ += offset, *this;
-  }
-
-  auto& operator-=(difference_type offset) {
-    return edge_iterator_ -= offset, *this;
-  }
-
-  const auto& operator[](difference_type offset) const {
-    return edge_iterator_[offset].vertex();
-  }
-
-  template <typename OtherEdgeIterator>
-  auto operator<(
-      const ConvexPolygonVertexIterator<OtherEdgeIterator>& other) const {
-    return edge_iterator_ < other.edge_iterator_;
-  }
-
-  template <typename OtherEdgeIterator>
-  auto operator<=(
-      const ConvexPolygonVertexIterator<OtherEdgeIterator>& other) const {
-    return edge_iterator_ <= other.edge_iterator_;
-  }
-
-  template <typename OtherEdgeIterator>
-  auto operator>=(
-      const ConvexPolygonVertexIterator<OtherEdgeIterator>& other) const {
-    return edge_iterator_ >= other.edge_iterator_;
-  }
-
-  template <typename OtherEdgeIterator>
-  auto operator>(
-      const ConvexPolygonVertexIterator<OtherEdgeIterator>& other) const {
-    return edge_iterator_ > other.edge_iterator_;
-  }
-
- private:
-  template <typename OtherEdgeIterator>
-  friend class ConvexPolygonVertexIterator;
-  EdgeIterator edge_iterator_;
+  using Transformer = ReturnVertexFromEdge<typename EdgeIterator::value_type>;
+  using Parent = TransformIterator<EdgeIterator, Transformer>;
+  using Parent::Parent;
 };
 
 }  // walnut
---
+++ include/walnut/transform_iterator.h
@@ -0,0 +1,142 @@
+#ifndef WALNUT_TRANSFORM_ITERATOR_H__
+#define WALNUT_TRANSFORM_ITERATOR_H__
+
+#include <type_traits>
+// For std::move
+#include <utility>
+
+namespace walnut {
+
+// An iterator that produces a reference to a subvalue of an aggregate type,
+// using a user provided function.
+template <typename InputIterator, typename Transformer>
+class TransformIterator {
+ public:
+  using difference_type = typename InputIterator::difference_type;
+  // The extra set of parenthesis around the argument to decltype are necessary
+  // so that the argument is considered an expression instead of an entity.
+  using value_type = typename std::remove_reference<decltype(
+        std::declval<Transformer>()(
+          std::declval<typename InputIterator::value_type>()))>::type;
+  using pointer = value_type*;
+  using reference = value_type&;
+  using iterator_category = typename InputIterator::iterator_category;
+
+  TransformIterator() = default;
+
+  explicit TransformIterator(InputIterator input_iterator) :
+    input_iterator_(std::move(input_iterator)) { }
+
+  TransformIterator(InputIterator input_iterator, Transformer transform) :
+    input_iterator_(std::move(input_iterator)),
+    transform_(std::move(transform)) { }
+
+  template <typename OtherInputIterator,
+            typename = std::enable_if_t<
+              std::is_constructible<InputIterator, OtherInputIterator>::value>>
+  TransformIterator(
+      const TransformIterator<OtherInputIterator, Transformer>& other) :
+    input_iterator_(other.input_iterator_),
+    transform_(other.transform_) { }
+
+  // Many of these functions return auto so that SFINAE kicks in and removes
+  // any operators that are not valid due to the input iterator type.
+
+  template <typename OtherInputIterator>
+  auto operator==(
+      const TransformIterator<OtherInputIterator, Transformer>& other) const {
+    return input_iterator_ == other.input_iterator_;
+  }
+
+  template <typename OtherInputIterator>
+  auto operator!=(
+      const TransformIterator<OtherInputIterator, Transformer>& other) const {
+    return input_iterator_ != other.input_iterator_;
+  }
+
+  reference operator*() const {
+    return transform_(*input_iterator_);
+  }
+
+  pointer operator->() const {
+    return &transform_(*input_iterator_);
+  }
+
+  TransformIterator& operator++() {
+    ++input_iterator_;
+    return *this;
+  }
+
+  TransformIterator operator++(int) {
+    return TransformIterator(input_iterator_++);
+  }
+
+  auto& operator--() {
+    --input_iterator_;
+    return *this;
+  }
+
+  auto operator--(int) {
+    return TransformIterator(input_iterator_--);
+  }
+
+  auto operator+(difference_type offset) const {
+    return TransformIterator(input_iterator_ + offset);
+  }
+
+  auto operator-(difference_type offset) const {
+    return TransformIterator(input_iterator_ - offset);
+  }
+
+  template <typename OtherInputIterator>
+  auto operator-(
+      const TransformIterator<OtherInputIterator, Transformer>& other) const {
+    return input_iterator_ - other.input_iterator_;
+  }
+
+  auto& operator+=(difference_type offset) {
+    return input_iterator_ += offset, *this;
+  }
+
+  auto& operator-=(difference_type offset) {
+    return input_iterator_ -= offset, *this;
+  }
+
+  auto& operator[](difference_type offset) const {
+    return transform_(input_iterator_[offset]);
+  }
+
+  template <typename OtherInputIterator>
+  auto operator<(
+      const TransformIterator<OtherInputIterator, Transformer>& other) const {
+    return input_iterator_ < other.input_iterator_;
+  }
+
+  template <typename OtherInputIterator>
+  auto operator<=(
+      const TransformIterator<OtherInputIterator, Transformer>& other) const {
+    return input_iterator_ <= other.input_iterator_;
+  }
+
+  template <typename OtherInputIterator>
+  auto operator>=(
+      const TransformIterator<OtherInputIterator, Transformer>& other) const {
+    return input_iterator_ >= other.input_iterator_;
+  }
+
+  template <typename OtherInputIterator>
+  auto operator>(
+      const TransformIterator<OtherInputIterator, Transformer>& other) const {
+    return input_iterator_ > other.input_iterator_;
+  }
+
+ private:
+  template <typename OtherInputIterator, typename OtherTransformer>
+  friend class TransformIterator;
+  InputIterator input_iterator_;
+  Transformer transform_;
+};
+
+}  // walnut
+
+#endif // WALNUT_TRANSFORM_ITERATOR_H__
